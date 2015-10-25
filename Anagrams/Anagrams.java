0b5b12b1fc85a8c48952dd64eaeed5e96a1cd833
egor90wd <egor90wd@gmail.com>
2015-10-26 00:51:20 +0200

    Anagrams

--- Anagrams/Anagrams.java
+++ Anagrams/Anagrams.java
@@ -7,14 +7,14 @@ public class Anagrams {
 	public static void main(String[] args) throws IOException {
 		reverseWords(readPhrase());		
 	}
-
+	/*Reading phrase from console*/
 	public static String readPhrase() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));		
 		String inputPhrase = reader.readLine();
 		reader.close();
 		return inputPhrase;
 	}
-
+	/*Reversing of words in phrase separated by space*/
 	public static void reverseWords(String phrase) {
 		String[] words = phrase.split(" ");
 		for (String string : words) {
